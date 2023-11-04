561e0f292402639d8c9dd67b252252b0fc01aa36
Team-Kawa <chuu.doe@proton.me>
2023-11-05 03:50:37 +0900

    Fixes: issues on install react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Nav from './Nav.js';
 
 function App() {
+
+  let data = "Hi from React!";
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav />;
+      <h1 style={ { color: 'skyblue' } }>{ data }</h1>
+    Bye Header:(
     </div>
   );
 }
---
+++ src/Nav.js
@@ -0,0 +1,18 @@
+import { Link } from 'react-router-dom';
+import React from 'react';
+import './App.css';
+
+function Nav() {
+  return (
+  <div>
+    <div className = "navb">
+      <Link className="navMenu" to={'/'}>Main</Link>
+      <Link className="navMenu" to={'/mypage'}>My page</Link>
+      <Link className="navMenu" to={'/contact'}>Contact us</Link>
+    </div>
+  </div>
+  );
+}
+
+// Nav 컴포넌트 수
+export default Nav;
