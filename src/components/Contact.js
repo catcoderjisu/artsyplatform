ada2f47c8644f58806c7ea89260eca36b4834f95
Team-Kawa <chuu.doe@proton.me>
2023-11-05 05:24:05 +0900

    (슬랙 연동 테스트)

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -7,7 +7,7 @@ function Contact() {
     <div>
       <h1 style={ { color: 'red' } }>
         <a href="https://www.notion.so/officialjoshua/project-NO-NAME-94d8c7b211f940cb9d6c4240ebc2db31?pvs=4" >
-          Our Team Notion
+          Team Notion
         </a>
       </h1>
       <a href = "https://github.com/Chiiikawa/noName_FE/tree/main">Github</a>
