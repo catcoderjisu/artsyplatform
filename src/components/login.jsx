b9b675572042e37432eccdbf097ce87ffbe7af78
Absolem <absolem.jk@gmail.com>
2023-11-10 10:49:39 +0900

    fix: login console

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,8 +1,8 @@
 // login page
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import axios from 'axios';
-import '../App.css'
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import "../App.css";
 
 function Login() {
   let isValidUsername = false;
@@ -15,8 +15,8 @@ function Login() {
   const navigate = useNavigate();
 
   // email, password 입력 시 화면에 render되도록 useState 사용
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const handleEmailChange = (e) => {
     setEmail(e.target.value);
@@ -29,21 +29,29 @@ function Login() {
   // 로그인 request를 서버로 보내는 함수
   async function handleLoginDataSubmit(e) {
     e.preventDefault();
+    console.log("email:", email);
+    console.log("password:", password);
+    console.log(
+      `${process.env.REACT_APP_SERVICE_URL}:${process.env.REACT_APP_BACKEND_PORT}/accounts/token/`,
+    );
     try {
       // .env를 바탕으로 backend 상대경로를 지정
-      const response = await axios.post(`${process.env.REACT_APP_SERVICE_URL}:${process.env.REACT_APP_BACKEND_PORT}/accounts/login`, {
-        email: email,
-        password: password
-      });
-      console.log('Login 진행 중:', response.data);
+      const response = await axios.post(
+        `${process.env.REACT_APP_SERVICE_URL}:${process.env.REACT_APP_BACKEND_PORT}/accounts/token/`,
+        {
+          email: email,
+          password: password,
+        },
+      );
+      console.log("Login 진행 중:", response.data);
     } catch (error) {
-      console.error('Authentication failed', error);
+      console.error("Authentication failed", error);
     }
   }
 
   // signup page로 이동시키는 함수
   const handleSignUpClick = () => {
-    navigate('../signup')
+    navigate("../signup");
   };
 
   return (
@@ -55,16 +63,24 @@ function Login() {
           <label>Email:</label>
           <input type="email" value={email} onChange={handleEmailChange} />
         </div>
-        <div className='formbox'>
+        <div className="formbox">
           <label>Password:</label>
-          <input type="password" value={password} onChange={handlePasswordChange} />
+          <input
+            type="password"
+            value={password}
+            onChange={handlePasswordChange}
+          />
         </div>
         <h3></h3>
-        <button type="submit" className="button01">Login</button>
-        <button type="button" className="button01" onClick={handleSignUpClick}>Sign Up</button>
+        <button type="submit" className="button01">
+          Login
+        </button>
+        <button type="button" className="button01" onClick={handleSignUpClick}>
+          Sign Up
+        </button>
       </form>
     </div>
-    );
-};
+  );
+}
 
 export default Login;
